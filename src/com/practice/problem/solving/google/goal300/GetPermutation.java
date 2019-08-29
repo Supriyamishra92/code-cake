89fb200d3e74b42f1088a9f4223ac04cd70a2956
Supriya Mishra <supriyamishra05992@gmail.com>
2019-08-30 00:06:10 +0530

    getpermutation

--- src/com/practice/problem/solving/google/goal300/GetPermutation.java
+++ src/com/practice/problem/solving/google/goal300/GetPermutation.java
@@ -1,4 +1,27 @@
 package com.practice.problem.solving.google.goal300;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GetPermutation {
+
+    public static String getPermutation(int n, int k) {
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while  ((line = input.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            line = input.readLine();
+            int k = Integer.parseInt(line);
+            String result = getPermutation(n,k);
+            String out = (result);
+            System.out.println(out);
+        }
+    }
 }
