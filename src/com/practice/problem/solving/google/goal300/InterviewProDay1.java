4500fe13734f6420dc319a0c02a57b669d38a70d
Supriya Mishra <supriyamishra05992@gmail.com>
2019-08-31 19:43:02 +0530

    added interviewPro assignments TODO

---
+++ src/com/practice/problem/solving/google/goal300/InterviewProDay1.java
@@ -0,0 +1,14 @@
+package com.practice.problem.solving.google.goal300;
+
+/***
+ * MICROSOFT :
+ *
+ * You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+public class InterviewProDay1 {
+}
---
+++ src/com/practice/problem/solving/google/goal300/InterviewProDay3.java
@@ -0,0 +1,15 @@
+package com.practice.problem.solving.google.goal300;
+
+/***
+ * TWITTER :
+ * A palindrome is a sequence of characters that reads the same backwards and forwards. Given a string, s, find the longest palindromic substring in s.
+ *
+ * Example:
+ * Input: "banana"
+ * Output: "anana"
+ *
+ * Input: "million"
+ * Output: "illi"
+ */
+public class InterviewProDay3 {
+}
