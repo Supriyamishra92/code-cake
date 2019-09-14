9fce382a629ddb7e2af0ec587ec83bc7457d3917
Supriya Mishra <supriyamishra05992@gmail.com>
2019-09-14 21:39:37 +0530

    TODO- Interview Pro

---
+++ src/com/practice/problem/solving/google/goal300/InterviewProDay11.java
@@ -0,0 +1,10 @@
+package com.practice.problem.solving.google.goal300;
+
+/***
+ * Apple:
+ *
+ * Given an integer k and a binary search tree, find the floor (less than or equal to) of k,
+ * and the ceiling (larger than or equal to) of k. If either does not exist, then print them as None.
+ */
+public class InterviewProDay11 {
+}
---
+++ src/com/practice/problem/solving/google/goal300/InterviewProDay12.java
@@ -0,0 +1,26 @@
+package com.practice.problem.solving.google.goal300;
+
+/***
+ * Twitter:
+ *
+ * You are given the root of a binary tree. Invert the binary tree in place.
+ * That is, all left children should become right children, and all right children should become left children.
+ *
+ * Example:
+ *
+ *     a
+ *    / \
+ *   b   c
+ *  / \  /
+ * d   e f
+ *
+ * The inverted version of this tree is as follows:
+ *
+ *   a
+ *  / \
+ *  c  b
+ *  \  / \
+ *   f e  d
+ */
+public class InterviewProDay12 {
+}
